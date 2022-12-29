1ff45d8578b9a197975588f85ee296407fbd99cd
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2022-12-29 13:23:35 +0100

    Added Fluxor

--- Website/Website.Client/Program.cs
+++ Website/Website.Client/Program.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Website.Client;
+using Fluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//Services
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+//Fluxor
+var currentAssembly = typeof(Program).Assembly;
+builder.Services.AddFluxor(opt => {
+    opt.ScanAssemblies(currentAssembly);
+#if DEBUG
+    opt.UseReduxDevTools();
+#endif
+});
+
+//Run
 await builder.Build().RunAsync();
