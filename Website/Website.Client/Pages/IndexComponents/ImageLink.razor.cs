b5206f84b6394dfe79aead5edc6a7f5094d4da67
Robin Claesson <robin.claesson@gmail.com>
2023-01-26 23:49:38 +0100

    Standardized items to components

---
+++ Website/Website.Client/Pages/IndexComponents/ImageLink.razor.cs
@@ -0,0 +1,19 @@
+﻿using Website.Client.Models;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using Website.Client.Store.View;
+
+namespace Website.Client.Pages.IndexComponents
+{
+    public partial class ImageLink
+    {
+        [Parameter]
+        public string Image { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Link { get; set; } = string.Empty;
+    }
+}
---
+++ Website/Website.Client/Shared/StandardCardTitle.razor.cs
@@ -0,0 +1,13 @@
+﻿using Website.Client.Models;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using Website.Client.Store.View;
+
+namespace Website.Client.Shared
+{
+    public partial class StandardCardTitle
+    {
+        [Parameter] 
+        public string Title { get; set; } = string.Empty;
+    }
+}
