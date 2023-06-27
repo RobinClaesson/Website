e1ef76c5f72b5f196e67f3aae3ae27485145d12f
Robin Claesson <robcla@kth.se>
2023-06-27 23:12:04 +0200

    Thesis project page

---
+++ Website/Website.Client/Pages/Projects/ProblemSolvingCode/Slides.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Website.Client.Pages.Projects.ProblemSolvingCode
+{
+    public partial class Slides
+    {
+        List<string> imageSources = new List<string>();
+
+        protected override void OnInitialized()
+        {
+            for (int i = 1; i <= 29; i++)
+            {
+                imageSources.Add($"\\img\\projects\\problemSolvingCode\\Presentation_page-{i}.jpg");
+            }
+            base.OnInitialized();
+        }
+    }
+}
